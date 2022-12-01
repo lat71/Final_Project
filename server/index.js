dfcf0d7dd400bf91ece4a8b41feccd43c566d191
lat71 <lat71@case.edu>
2022-12-01 13:24:05 -0500

    began backend setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+
+import routes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', routes);
+
+app.use(bodyParser.json({ "limit": "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ "limit": "30mb", extended: true}));
+app.use(cors());
+
+const CONECTION_URL = 'mongodb+srv://Admin123:abc@vue-express.zgopmut.mongodb.net/test'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONECTION_URL, { useNewUrlParser: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false)
