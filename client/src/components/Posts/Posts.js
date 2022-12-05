6944a0ea4a593b4d54c2ab3cadd1bce244462fac
lat71 <lat71@case.edu>
2022-12-05 00:33:54 -0500

    Implemented Post component, posts now show on front end

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Post from "./Post/Post";
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+
 import useStyles from './styles';
 
 const Posts = () => {
     const styles = useStyles();
     const posts = useSelector((state) => state.posts);
-
-    console.log(posts);
     
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={styles.container} container aligItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post.id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
