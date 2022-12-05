eaab97650c2d10e705189c9a5500475a7abf049b
lat71 <lat71@case.edu>
2022-12-04 23:52:17 -0500

    front end submit form section

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import useStyles from './styles';
-
+import { useDispatch } from 'react-redux';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-
+import { getPosts } from './actions/posts'
 import mainimage from './images/mainimage.png'
 
 const App = () => {
     const styles = useStyles();
+    const dispatch = useDispatch();
 
+    useEffect(() => 
+        {dispatch(getPosts());}, [dispatch]);
     return (
         <Container maxwidth="lg">
             <AppBar className={styles.appBar} position="static" color="inherit">
@@ -18,7 +21,7 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import routes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', routes);
-
 app.use(bodyParser.json({ "limit": "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ "limit": "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', routes);
 
 const CONECTION_URL = 'mongodb+srv://Admin123:abc@vue-express.zgopmut.mongodb.net/test'
 const PORT = process.env.PORT || 5000;
