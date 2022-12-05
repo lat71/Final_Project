74884631d152c70856c8d085ffc78d9a171b3b91
lat71 <lat71@case.edu>
2022-12-04 23:02:43 -0500

    Beginning work on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import useStyles from './styles';
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import mainimage from './images/mainimage.png'
 
 const App = () => {
+    const styles = useStyles();
+
     return (
-        <div>
-            <h1>App Here</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar className={styles.appBar} position="static" color="inherit">
+                <Typography className={styles.heading} variant="h2" align="center">Memories</Typography>
+                <img className={styles.image} src={mainimage} alt="mainimage" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow> 
+        </Container>
     );
 }
 
